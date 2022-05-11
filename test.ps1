dcd284303a18f3cec257d1394abc2abb5ffb8165
wkway <wkway@mail.lv>
2022-05-11 03:41:01 -0700

    first commit

---
+++ test.ps1
@@ -0,0 +1,18 @@
+iwr -useb -Uri "https://raw.hellogithub.com/hosts" -o 1
+#iwr -useb -Uri "https://raw.githubusercontent.com/521xueweihan/GitHub520/main/hosts" -o 1
+type 1 >myhost
+iwr -useb -Uri "https://gitee.com/ineo6/hosts/raw/master/next-hosts" -o 2
+#iwr -useb -Uri "https://raw.githubusercontent.com/ineo6/hosts/master/hosts" -o 2
+type 2 >>myhost
+Get-Content myhost | Out-File myhosts -Encoding ascii
+rm 1
+rm 2
+rm myhost
+mv myhosts hosts/
+cp blacklist hosts/
+mv build hosts/
+mv Brave hosts/data/
+mv WindowsSpyBlocker.extra hosts/data/
+mv WindowsSpyBlocker.spy hosts/data/
+mv WindowsSpyBlocker.update hosts/data/
+mv yhosts hosts/data/
