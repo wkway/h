22bea94275f985672b7ca3ee2c6abb4412bf4042
wkway <104142595+wkway@users.noreply.github.com>
2022-05-13 04:14:28 -0700

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,13 +1,14 @@
-iwr -useb -Uri "https://raw.hellogithub.com/hosts" -o 1
+#iwr -useb -Uri "https://raw.hellogithub.com/hosts" -o 1
 #iwr -useb -Uri "https://raw.githubusercontent.com/521xueweihan/GitHub520/main/hosts" -o 1
-type 1 >myhost
-iwr -useb -Uri "https://gitee.com/ineo6/hosts/raw/master/next-hosts" -o 2
+#type 1 >myhost
+#iwr -useb -Uri "https://gitee.com/ineo6/hosts/raw/master/next-hosts" -o 2
 #iwr -useb -Uri "https://raw.githubusercontent.com/ineo6/hosts/master/hosts" -o 2
-type 2 >>myhost
-Get-Content myhost | Out-File myhosts -Encoding ascii
-rm 1
-rm 2
-rm myhost
+#type 2 >>myhost
+#Get-Content myhost | Out-File myhosts -Encoding ascii
+#rm 1
+#rm 2
+#rm myhost
+iwr -useb -Uri "https://raw.githubusercontent.com/wkway/gh/master/hosts" -o myhosts
 mv myhosts hosts/
 cp blacklist hosts/
 mv build hosts/
